155be3f061a8b935d170ca3a561fb6b97d36e228
nanahuse <109294061+Nanahuse@users.noreply.github.com>
2026-09-20 20:50:46 +0900

    Initialize Version 2 Python project

---
+++ src/quickreplay/__init__.py
@@ -0,0 +1,7 @@
+"""QuickReplay Version 2.
+
+Version 2 is under development.  This package currently provides only the
+project skeleton; no product functionality is implemented yet.
+"""
+
+__version__ = "2.0.0.dev0"
